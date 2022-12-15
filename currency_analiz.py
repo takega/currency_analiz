2450f9cb9afc7fde1be1f5fbdadcdf3fe1597f6e
takega <opex@midoma.ru>
2022-12-15 17:44:51 +0300

    optimization

--- currency_analiz.py
+++ currency_analiz.py
@@ -4,37 +4,40 @@ from datetime import datetime, timedelta
 from pandas import ExcelWriter
 import xmltodict as xmltodict
 from pprint import pprint
+currences=['KRW','KGS','USD','CNY']
 
-
-def currency_today():
+def currency_today(valute):
     # Делаем запрос к Центробанку
     url = 'http://www.cbr.ru/scripts/XML_daily.asp?date_req='
     date = datetime.now()
     date = date.strftime('%d/%m/%Y')
-
     response = requests.get(url + date)
     tree = xmltodict.parse(response.content)
     date = datetime.strptime(date, '%d/%m/%Y')
+    #pprint(tree['ValCurs']['Valute'])
     # Создаем таблицу с нужными данными
     df = pd.DataFrame(tree['ValCurs']['Valute'])
     df['Date'] = tree['ValCurs']['@Date']
-    df = df.set_index(['@ID'])
-    return df.loc[['R01815']], df.loc[['R01370']]
+    df = df.set_index(['CharCode'])
+    return df.loc[valute]
 
-
+#print(currency_today('USD'))
 # Скачиваем файл с данными
-df_VON = pd.read_excel('currency.xlsx   ', sheet_name='VON', index_col='@ID')
-df_SOM = pd.read_excel('currency.xlsx', sheet_name='SOM',index_col='@ID')
-VON_today, SOM_today = currency_today()
-df_VON = pd.concat([df_VON,VON_today])
-df_SOM = pd.concat([df_SOM,SOM_today])
-df_VON = df_VON.drop_duplicates()
-df_SOM = df_SOM.drop_duplicates()
+def storage(name):
+    cur_name = name
+    file = name+'.xlsx'
+    name = pd.read_excel(file, index_col='CharCode')
+    name=name.append(currency_today(cur_name))
+    #name = pd.concat([name, currency_today(cur_name)])
+    name = name.drop_duplicates()
+    name.to_excel(file)
+    return name
 
-print(df_VON, df_SOM)
-# "Записываем в Excel"
-with ExcelWriter('currency.xlsx') as writer:
-    df_VON.to_excel(writer, sheet_name='VON')
-    df_SOM.to_excel(writer, sheet_name='SOM')
+for currency in currences:
+    storage(currency)
 
-# ttp://www.cbr.ru/scripts/XML_daily.asp?date_req=15/12/2022
+
+print(storage('KRW'))
+
+#print(df_VON)
+#df_VON, df_SOM, df_USD, df_CNY = currency_today()
---
+++ first_time.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import requests
+from datetime import datetime, timedelta
+from pandas import ExcelWriter
+import xmltodict as xmltodict
+from pprint import pprint
+
+currences=['KRW','KGS','USD','CNY']
+url = 'http://www.cbr.ru/scripts/XML_daily.asp?date_req='
+date = datetime.now()-timedelta(days=30)
+result_df=pd.DataFrame()
+for i in range(30):
+    date = date.strftime('%d/%m/%Y')
+    response = requests.get(url + date)
+    tree = xmltodict.parse(response.content)
+    date = datetime.strptime(date, '%d/%m/%Y')
+    date = date + timedelta(days=1)
+    #print(tree['ValCurs'])
+    # Создаем таблицу с нужными данными
+    df = pd.DataFrame(tree['ValCurs']['Valute'])
+    df['Date'] = tree['ValCurs']['@Date']
+    df = df.set_index(['CharCode'])
+    result_df=result_df.append(df.loc[currences])
+for name in currences:
+    file = name + '.xlsx'
+    result_df.loc[name].to_excel(file)
+
+
+
